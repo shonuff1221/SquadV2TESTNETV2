ebf34bf8d3566ea2c734641eaf01b8525c238fad
Stephon <shonuff4521@gmail.com>
2021-04-18 14:54:53 -0400

    Update exchange.sol
    New Contract

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -1,136 +1,358 @@
+
 pragma solidity ^0.6.0;
 import "./Squad.sol";
-contract Owned {
-    modifier onlyOwner() {
-        require(msg.sender==owner,"you are not a owner");
+contract squadUpv2Exchange is SquadUpV2 {
+    /*=================================
+    =            MODIFIERS            =
+    =================================*/
+    // only people with tokens
+    modifier onlybelievers () {
+        require(balanceOf(msg.sender) > 0);
         _;
     }
     
-    address payable public owner;
-    function changeOwner(address payable _newOwner) public onlyOwner {
-        require(_newOwner!=address(0));
-        owner = _newOwner;
+    // admin can:
+    // -> change the name of the contract
+    // -> change the name of the token
+    // -> change the PoS difficulty 
+    // they CANNOT:
+    // -> take funds
+    // -> disable withdrawals
+    // -> kill the contract
+    // -> change the price of tokens
+    modifier onlyAdmin(){
+        require(msg.sender==owner,"Only can owner can call!");
+        _;
+    }
+    /*==============================
+    =            EVENTS            =
+    ==============================*/
+    event onTokenPurchase(
+        address indexed customerAddress,
+        uint256 incomingEthereum,
+        uint256 tokensMinted,
+        address indexed referredBy
+    );
+    
+    event onTokenSell(
+        address indexed customerAddress,
+        uint256 tokensBurned,
+        uint256 ethereumEarned
+    );
+    
+    event onWithdraw(
+        address indexed customerAddress,
+        uint256 ethereumWithdrawn
+    );
+    
+    // ERC20
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 tokens
+    );
+    
+    
+    /*=====================================
+    =            CONFIGURABLES            =
+    =====================================*/
+    address payable owner;
+    uint256 constant internal dividendFee_ = 8*(10**36);
+    uint256 constant internal refferalBonus=2*(10**36);
+    uint256 constant internal tokenPriceInitial_ =0.005 ether;
+    uint256 constant internal tokenPriceIncremental_ = 0.001 ether;
+    uint256 constant internal magnitude = 2**64;
+    
+    // proof of stake (defaults at 1 token)
+
+    // ambassador program
+    mapping(address => bool) internal ambassadors_;
+    uint256 constant internal ambassadorMaxPurchase_ = 1 ether;
+    uint256 constant internal ambassadorQuota_ = 1 ether;
+    
+    
+    
+   /*================================
+    =            DATASETS            =
+    ================================*/
+    // amount of shares for each address (scaled number)
+    mapping(address => int256) internal payoutsTo_;
+    mapping(address => uint256) internal ambassadorAccumulatedQuota_;
+    uint256 internal profitPerShare_;
+    
+    // administrator list (see above on what they can do)
+    mapping(bytes32 => bool) public administrators;
+    
+    
+    bool public onlyAmbassadors = false;
+    
+
+
+    /*=======================================
+    =            PUBLIC FUNCTIONS            =
+    =======================================*/
+    /*
+    * -- APPLICATION ENTRY POINTS --  
+    */
+    constructor()
+        public
+    {
+        // add administrators here
+        administrators[0x9bcc16873606dc04acb98263f74c420525ddef61de0d5f18fd97d16de659131a] = true;
+						 
+   
+        ambassadors_[0x0000000000000000000000000000000000000000] = true;
+        
+        
+        owner=msg.sender;
+                       
     }
     
-}
-contract SquadUpExchange is squadUpv2,Owned{
-     constructor()public{
-     startTime=now;
-     owner=msg.sender;
-     addTokenHolder(owner);
-    }
+     
     /**
-    * @notice A method to check if an address is a stakeholder.
-    * @param _address The address to verify.
-    * @return bool, uint256 Whether the address is a stakeholder,
-    * and if so its position in the tokenHolders array.
-    */
-   function isTokenHolder(address _address)
-       public
-       view
-       returns(bool, uint256)
-   {
-       for (uint256 s = 0; s < tokenHolders.length; s += 1){
-           if (_address == tokenHolders[s]) return (true, s);
-       }
-       return (false, 0);
-   }
-
-   /**
-    * @notice A method to add a stakeholder.
-    * @param _stakeholder The stakeholder to add.
-    */
-   function addTokenHolder(address _stakeholder)
-       public
-   {
-       (bool _isStakeholder, ) = isTokenHolder(_stakeholder);
-       if(!_isStakeholder) tokenHolders.push(_stakeholder);
-   }
-
-   /**
-    * @notice A method to remove a stakeholder.
-    * @param _stakeholder The stakeholder to remove.
-    */
-   function removeTokenHolder(address _stakeholder)
-       public
-   {
-       (bool _isStakeholder, uint256 s) = isTokenHolder(_stakeholder);
-       if(_isStakeholder){
-           tokenHolders[s] = tokenHolders[tokenHolders.length - 1];
-           tokenHolders.pop();
-       }
-   }
-    using SafeMath for uint256;
-    struct Users{
-        uint256 stakeHolderBonus;
-        uint256 refferalBonus;
-        address payable upline;
+     * Converts all incoming Ethereum to tokens for the caller, and passes down the referral address (if any)
+     */
+    function buy(address payable _referredBy)
+        public
+        payable
+        returns(uint256)
+    {
+        purchaseTokens(msg.value, _referredBy);
     }
-    mapping(address=>Users)public users;
-    uint256 public startTime;
-    address[] public tokenHolders;
-    uint256 public totalTokenMinted;
-    uint256 public totalTokenSold;
-    using SafeMath for uint256;
-    uint256 initialPrice=0.005 ether;
-    function buyPriceCalculation() public view returns(uint){
-        require(startTime != 0,"contract isn't deployed yet!");
-        uint256 increment= ((now - startTime)/(10 hours))*10e13;
-        return increment;
+    
+    /**
+     * Liquifies tokens to ethereum.
+     */
+    function sell(uint256 _amountOfTokens)
+        onlybelievers ()
+        public
+    {
+      
+        address payable _customerAddress = msg.sender;
+       
+        require(_amountOfTokens <= balanceOf(_customerAddress));
+        uint256 _tokens = _amountOfTokens;
+        uint256 _ethereum = tokensToEthereum_(_tokens);
+        // burn the sold tokens
+        _burn(_customerAddress,_amountOfTokens);
+        //send bnb back to seller
+        _customerAddress.transfer(_ethereum);
+        // fire event
+        onTokenSell(_customerAddress, _tokens, _ethereum);
     }
-    function buyPrice(uint256 _BNBValue)public view returns(uint256){
-       _BNBValue=_BNBValue-((_BNBValue.mul(10)).div(100));
-       uint256 price= (buyPriceCalculation()+initialPrice);
-       return (_BNBValue.div(price)).mul(1e18);
+    /*----------  ADMINISTRATOR ONLY FUNCTIONS  ----------*/
+    /**
+     * administrator can manually disable the ambassador phase.
+     */
+    function disableInitialStage()
+        onlyAdmin()
+        public
+    {
+        onlyAmbassadors = false;
     }
-    function setReferral(address payable _upline,address _add)internal{
-        
-        require(_upline==owner||0!=balanceOf(_upline) && _upline!=address(0),"Upline doesn't exist!");
-        users[_add].upline=_upline;
+    
+   
+    function setAdministrator(bytes32 _identifier, bool _status)
+        onlyAdmin()
+        public
+    {
+        administrators[_identifier] = _status;
     }
-    function buyToken(uint256 _BNBValue,address payable _refferedBy)public payable returns(bool){
-     require(msg.value==_BNBValue,"Invalid price");
-     require(buyPrice(_BNBValue)>0,"Invalid amount of token selected");
-     _mint(msg.sender,buyPrice(_BNBValue));
-     totalTokenMinted+=buyPrice(_BNBValue);
-     setReferral(_refferedBy,msg.sender);
-     _refferedBy.transfer(SafeMath.mul(_BNBValue,2).div(100));
-     uint256 TotalstakeholderBonus=SafeMath.mul(_BNBValue,8).div(100);
-     owner.transfer(TotalstakeholderBonus);
-     addTokenHolder(msg.sender);
-    }
-    function marketCap()public view returns(uint256){
+    /*----------  HELPERS AND CALCULATORS  ----------*/
+    /**
+     * Method to view the current Ethereum stored in the contract
+     * Example: totalEthereumBalance()
+     */
+    function totalEthereumBalance()
+        public
+        view
+        returns(uint)
+    {
         return address(this).balance;
     }
-    function circulatingSupply()public view returns(uint256){
-        return SafeMath.sub(totalTokenMinted,totalTokenSold);
+    
+    /**
+     * Return the buy price of 1 individual token.
+     */
+    function sellPrice() 
+        public 
+        view 
+        returns(uint256)
+    {
+       
+        if(totalSupply() == 0){
+            return tokenPriceInitial_ - tokenPriceIncremental_;
+        } else {
+            uint256 _ethereum = tokensToEthereum_(1e18);
+            uint256 _dividends = SafeMath.div(_ethereum, dividendFee_  );
+            uint256 _taxedEthereum = SafeMath.sub(_ethereum, _dividends);
+            return _taxedEthereum;
+        }
     }
-    function sellPriceCalculation()public view returns(uint256){
-        require(circulatingSupply()>0,"No token bought yet");
-        uint256 marketCapValue=SafeMath.mul(marketCap(),10e18);
-        uint256 price= SafeMath.div(marketCapValue,circulatingSupply());
-        return price;
+    
+    /**
+     * Return the sell price of 1 individual token.
+     */
+    function buyPrice() 
+        public 
+        view 
+        returns(uint256)
+    {
+        
+        if(totalSupply() == 0){
+            return tokenPriceInitial_ + tokenPriceIncremental_;
+        } else {
+            uint256 _ethereum = tokensToEthereum_(1e18);
+            uint256 _dividends = SafeMath.div(_ethereum, dividendFee_  );
+            uint256 _taxedEthereum = SafeMath.add(_ethereum, _dividends);
+            return _taxedEthereum;
+        }
     }
-    function sellPrice(uint256 _numberOfTokens)public view returns(uint256){
-        uint256 price=SafeMath.mul(_numberOfTokens,sellPriceCalculation());
-        return SafeMath.div(price,10e18);
+    
+   
+    function calculateTokensReceived(uint256 _incomingEthereum) 
+        public 
+        view 
+        returns(uint256)
+    {
+        uint256 refferalBonus=SafeMath.div((refferalBonus*1),_incomingEthereum);
+        uint256 adminFee=SafeMath.div((dividendFee_*1),_incomingEthereum);
+        uint256 totalFee=SafeMath.add(adminFee,refferalBonus);
+        uint256 _taxedEthereum = SafeMath.sub(_incomingEthereum, totalFee);
+        uint256 _amountOfTokens = ethereumToTokens_(_taxedEthereum);
+        return _amountOfTokens;
     }
-    function sellToken(uint256 _numberOfTokens)public returns(bool){
-        require(balanceOf(msg.sender)>=_numberOfTokens,"you dont have enough tokens!");
-        uint256 price=sellPrice(_numberOfTokens);
-        require(price>0,"Price is not set!");
-        require(address(this).balance>=price,"Contract hasn't balance yet!");
-        _burn(msg.sender,_numberOfTokens);
-        totalTokenSold+=_numberOfTokens;
-        uint256 refferalBonus=SafeMath.mul(price,2).div(100);
-        uint256 TotalstakeholderBonus=SafeMath.mul(price,8).div(100);
-        uint256 sellReturn=price.sub(refferalBonus.add(TotalstakeholderBonus));
-         users[msg.sender].upline.transfer(refferalBonus);
-         owner.transfer(TotalstakeholderBonus);
-         msg.sender.transfer(sellReturn);
-        if(balanceOf(msg.sender)==0){
-         removeTokenHolder(msg.sender);
+    
+   
+    function calculateEthereumReceived(uint256 _tokensToSell) 
+        public 
+        view 
+        returns(uint256)
+    {
+        require(_tokensToSell <= totalSupply());
+        uint256 _ethereum = tokensToEthereum_(_tokensToSell);
+        return _ethereum;
+    }
+    
+    
+    /*==========================================
+    =            INTERNAL FUNCTIONS            =
+    ==========================================*/
+    function purchaseTokens(uint256 _incomingEthereum, address payable _referredBy)
+        internal
+        returns(uint256)
+    {
+        // data setup
+        address _customerAddress = msg.sender;
+        uint256 refferalBonus=SafeMath.div((refferalBonus*1),_incomingEthereum);
+        uint256 adminFee=SafeMath.div((dividendFee_*1),_incomingEthereum);
+        uint256 totalFee=SafeMath.add(adminFee,refferalBonus);
+        uint256 _taxedEthereum = SafeMath.sub(_incomingEthereum, totalFee);
+        uint256 _amountOfTokens = ethereumToTokens_(_taxedEthereum);
+
+      
+        require(_amountOfTokens > 0 && (SafeMath.add(_amountOfTokens,totalSupply()) > totalSupply()));
+        
+        // is the user referred by a karmalink?
+        if(
+            // is this a referred purchase?
+            _referredBy != 0x0000000000000000000000000000000000000000 &&
+
+            // no cheating!
+            _referredBy != _customerAddress &&
+            
+        
+            balanceOf(_referredBy)>0||
+            _referredBy==owner
+        ){
+            // wealth redistribution
+            _referredBy.transfer(refferalBonus);
+        }
+        
+        // update  the ledger address for the customer
+        _mint(_customerAddress,_amountOfTokens);
+        owner.transfer(adminFee);
+        // fire event
+        onTokenPurchase(_customerAddress, _incomingEthereum, _amountOfTokens, _referredBy);
+        
+        return _amountOfTokens;
+    }
+
+    /**
+     * Calculate Token price based on an amount of incoming ethereum
+     * It's an algorithm, hopefully we gave you the whitepaper with it in scientific notation;
+     * Some conversions occurred to prevent decimal errors or underflows / overflows in solidity code.
+     */
+    function ethereumToTokens_(uint256 _ethereum)
+        internal
+        view
+        returns(uint256)
+    {
+        uint256 _tokenPriceInitial = tokenPriceInitial_ * 1e18;
+        uint256 _tokensReceived = 
+         (
+            (
+                // underflow attempts BTFO
+                SafeMath.sub(
+                    (sqrt
+                        (
+                            (_tokenPriceInitial**2)
+                            +
+                            (2*(tokenPriceIncremental_ * 1e18)*(_ethereum * 1e18))
+                            +
+                            (((tokenPriceIncremental_)**2)*(totalSupply()**2))
+                            +
+                            (2*(tokenPriceIncremental_)*_tokenPriceInitial*totalSupply())
+                        )
+                    ), _tokenPriceInitial
+                )
+            )/(tokenPriceIncremental_)
+        )-(totalSupply())
+        ;
+  
+        return _tokensReceived;
+    }
+    
+    /**
+     * Calculate token sell value.
+          */
+     function tokensToEthereum_(uint256 _tokens)
+        internal
+        view
+        returns(uint256)
+    {
+
+        uint256 tokens_ = (_tokens + 1e18);
+        uint256 _tokenSupply = (totalSupply() + 1e18);
+        uint256 _etherReceived =
+        (
+            // underflow attempts BTFO
+            SafeMath.sub(
+                (
+                    (
+                        (
+                            tokenPriceInitial_ +(tokenPriceIncremental_ * (_tokenSupply/1e18))
+                        )-tokenPriceIncremental_
+                    )*(tokens_ - 1e18)
+                ),(tokenPriceIncremental_*((tokens_**2-tokens_)/1e18))/2
+            )
+        /1e18);
+        return _etherReceived;
+    }
+    
+    
+    
+    function sqrt(uint x) internal pure returns (uint y) {
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
         }
     }
 }
-  
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
